1f0b3ab1ffe1129af279cb95acfeccb3ae66f3c1
27tr <114559221+MCDream3@users.noreply.github.com>
2022-11-23 08:24:46 +0000

    dodano kilka tablic

---
+++ Tablice.cs
@@ -0,0 +1,56 @@
+using System;
+
+//tablice - zmienna (kolekcja) do utrzymania wielu zmiennych
+/*int a = 3;
+int b = 5;
+int c = 6;
+int d = 7;
+int e;
+int[] tablica = new int[4];
+tablica[0]=3;
+tablica[1]=5;
+tablica[2]=6;
+tablica[3]=7;
+tablica[4]=8;
+Console.WriteLine(tablica[3]+ ""+ tablica[2]);
+
+int[] tablice = new int[4];
+int i = 0;
+for (i = 0;i<4;i++)
+{
+  Console.WriteLine(tablice[i]);
+}*/
+/*int[]K = {2,3,5,8,13,21,34,55};
+for (int i = 0;i<K.Length;i++)
+{
+  Console.WriteLine($"{K[i]}");
+}*/
+
+//int[] L;
+//L = new int[]{1,3,5};
+
+//znajdż maksymalną wartość w tablicy [4,5,8,9,7,6]
+/*int[] G = new int[] {4,5,8,9,7,6};
+int i = 1;
+int max = G[0];
+for(i=1;i<G.Length;i++)
+{
+  if(G[i]>max)
+  {
+    max=G[i];
+  }
+}
+Console.WriteLine(max);
+*/
+/*int[]T = new int[]{4,5,8,9,7,6};
+int i = 1;
+int min = T[0];
+for(i = 1;i<T.Length;i++)
+{
+  if(T[i]<min)
+  {
+    min = T[i];
+  }
+}
+Console.WriteLine(min)
+*/
