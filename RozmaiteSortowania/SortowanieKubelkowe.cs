734196ca93547d5f4ea5bc1f854fa1f27a6105f4
27tr <114559221+MCDream3@users.noreply.github.com>
2023-04-19 19:33:08 +0200

    Create SortowanieKubelkowe.cs
    dodano sortowanie kubełkowe

---
+++ RozmaiteSortowania/SortowanieKubelkowe.cs
@@ -0,0 +1,45 @@
+//~Casual~
+
+class BucketSort
+{
+    public static List<int> Sort(params int[] x)
+    {
+        List<int> sortedArray = new List<int>();
+        int numOfBuckets = 10;
+        List<int>[] buckets = new List<int>[numOfBuckets];
+        for (int i = 0; i < numOfBuckets; i++)
+        {
+            buckets[i] = new List<int>();
+        }
+        for (int i = 0; i < x.Length; i++)
+        {
+            int bucket = (x[i] / numOfBuckets);
+            buckets[bucket].Add(x[i]);
+        }
+        for (int i = 0; i < numOfBuckets; i++)
+        {
+            List<int> temp = InsertionSort(buckets[i]);
+            sortedArray.AddRange(temp);
+        }
+        return sortedArray;
+    }
+//Wstawienie Insertion Sort
+public static List<int> InsertionSort(List<int> input)
+{
+    for (int i = 1; i < input.Count; i++)
+    {
+        int currentValue = input[i];
+        int pointer = i - 1;
+        while (pointer >= 0)
+        {
+            if (currentValue < input[pointer])
+            {
+                input[pointer + 1] = input[pointer];
+                input[pointer] = currentValue;
+            }
+            else break;
+        }
+    }
+    return input;
+}
+}
