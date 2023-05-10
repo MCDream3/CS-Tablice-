90c438224961e0f400315addee43f870e269967b
27tr <114559221+MCDream3@users.noreply.github.com>
2023-05-10 07:29:37 +0200

    Usunięto pozostałą metodę

--- RozmaiteSortowania/SortowanieBabelkowe.cs
+++ RozmaiteSortowania/SortowanieBabelkowe.cs
@@ -1,21 +1,3 @@
-//1. Sortowanie bąbelkowe                   <-- bubble sort -->
-//~Casual "jako moja metoda"~
-
-static void
-BubbleSort(IComparable[] a)                     //<--Comparable czyli porównywalny
-{
-    int n = a.Length;
-    for(int i = 0; i < n; i++)
-        for (int j = n - 1; j >= i; j--)
-            if (a[j - 1].CompareTo(a[j]) > 0)
-            {
-                IComparable x = a[j - 1];
-                a[j - 1] = a[j];
-                a[j] = x;
-            }
-}
-
-//~TP_POpr method XD~       od prawej rosnąco*
 
 int Temp;
 for (int i = n-1; i > 0; i--)
