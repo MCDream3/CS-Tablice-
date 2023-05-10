90172c91c1636e3eb8d2187d55348e0f9106de64
27tr <114559221+MCDream3@users.noreply.github.com>
2023-05-10 07:27:11 +0200

    Usunięto pozostałą metodę

--- RozmaiteSortowania/SortowaniePrzezWstawianie.cs
+++ RozmaiteSortowania/SortowaniePrzezWstawianie.cs
@@ -1,20 +1,3 @@
-//3. Sortowanie przez wstawianie          <--insertion sort-->            sortowania w czasie liniowym
-//~Casual~
-
-void InsertSort(int []tab, int size)
-{
-    int temp, j;
-    for (int i = 1; i < size; i++)
-    {
-        temp = tab[i];
-        for (int k = i - 1; k >= 0 && tab[j] > temp; j--)
-            tab[k + 1] = tab[k];
-        tab[j + 1] = temp;
-    }
-}
-
-//~TP_POpr method~
-
 int j, TEMP;
 for (int i = 1; i < n; i++)
 {
